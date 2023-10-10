7a083e840f38320ca12873028ccb81fbaa6c3173
Amirhossein <amirmsh@student.ubc.ca>
2023-10-09 20:31:55 -0700

    -completed left portion of topbar, but the link is not working

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,24 @@
 import "./topbar.scss"
+import {Person, Mail} from "@mui/icons-material"
 
 export default function Topbar() {
   return (
     <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">genius.</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+1 604-861-4711</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>amirmsh@student.ubc.ca</span>
+          </div>
+        </div>
+        <div className="right">
+        </div>
+      </div>
 
     </div>
   )

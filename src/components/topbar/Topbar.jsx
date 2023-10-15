8e879a22a789bebd2adb6dd6bbb0cd0d9ae15af9
Amirhossein <amirmsh@student.ubc.ca>
2023-10-14 21:54:10 -0700

    made the topbar links to email and phone

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,11 +9,14 @@ export default function Topbar({menuOpen, setMenuOpen}) {
           <a href="#intro" className="logo">genius.</a>
           <div className="itemContainer">
             <Person className="icon"/>
-            <span>+1 604-861-4711</span>
+            <a href="tel:+16048614711"><span>+1 604-861-4711</span></a>
+            
           </div>
           <div className="itemContainer">
             <Mail className="icon"/>
-            <span>amirmsh@student.ubc.ca</span>
+            <a href="mailto:amirmsh@student.ubc.ca">
+              <span>amirmsh@student.ubc.ca</span>
+            </a>
           </div>
         </div>
         <div className="right">

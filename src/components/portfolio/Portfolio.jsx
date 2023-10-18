a72dfe6fd5a7a511e634198ef5b9762fced706e1
Amirhossein <amirmsh@student.ubc.ca>
2023-10-17 23:51:25 -0700

    Finished portfolio sec

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react"
 import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
 const [selected, setSelected] = useState("Featured");
+const [data, setData] = useState([]);
 
 const list = [
   {
@@ -26,7 +34,29 @@ const list = [
     id: "content",
     title: "Content",
   }
-]
+];
+
+useEffect(() => {
+  switch (selected) {
+    case "featured":
+      setData(featuredPortfolio);
+      break;
+    case "web":
+      setData(webPortfolio);
+      break;
+    case "mobile":
+      setData(mobilePortfolio);
+      break;
+    case "design":
+      setData(designPortfolio);
+      break;
+    case "content":
+      setData(contentPortfolio);
+      break;
+    default:
+      setData(featuredPortfolio);
+  }
+}, [selected]);
 
   return (
     <div className="portfolio" id="portfolio">
@@ -42,8 +72,13 @@ const list = [
         ))}
       </ul>
       <div className="container">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+
+          </div>
+        ))}
       </div>
     </div>
   );

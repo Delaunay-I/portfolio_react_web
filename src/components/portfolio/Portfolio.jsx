9dddb72ea3be72ca008c2273623b2fc41c20508c
Amirhossein <amirmsh@student.ubc.ca>
2023-10-17 23:09:16 -0700

    Adding portfolio details

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,18 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          <li>Featured</li>
+          <li>Web App</li>
+          <li>Mobile App</li>
+          <li>Design</li>
+          <li>Branding</li>
+        </ul>
+        <div className="container">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
     </div>
   )
 }

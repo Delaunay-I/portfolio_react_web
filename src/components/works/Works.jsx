33d1439b06fe1e8b00567b21d38e9509b89660e3
Amirhossein <amirmsh@student.ubc.ca>
2023-10-18 21:56:33 -0700

    added details of works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,29 @@
-import "./works.scss"
+import "./works.scss";
 
 export default function Works() {
   return (
     <div className="works" id="works">
-        
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  In the world of mechanical engineering, innovation is the
+                  driving force behind progress. As a master's student, I'm
+                  passionate about CFD, control, and stability of solvers.
+                </p>
+                <span>Project</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
